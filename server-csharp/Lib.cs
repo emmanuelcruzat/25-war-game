d8e4fc853e8c0b9047397840e89f124b7ad35fa3
Emmanuel Cruzat <etc@njit.edu>
2025-09-13 10:03:47 -0400

    Reformatted directory

---
+++ server-csharp/Lib.cs
@@ -0,0 +1,74 @@
+using SpacetimeDB;
+
+public static partial class Module
+{
+    // We're using this table as a singleton, so in this table
+    // there will only be one element where the `id` is 0.
+    [Table(Name = "config", Public = true)]
+    public partial struct Config
+    {
+        [PrimaryKey]
+        public uint id;
+        public ulong world_size;
+    }
+
+    [Table(Name = "entity", Public = true)]
+    public partial struct Entity
+    {
+        [PrimaryKey, AutoInc]
+        public uint entity_id;
+        public DbVector2 position;
+        public uint mass;
+    }
+
+    [Table(Name = "circle", Public = true)]
+    public partial struct Circle
+    {
+        [PrimaryKey]
+        public uint entity_id;
+        [SpacetimeDB.Index.BTree]
+        public uint player_id;
+        public DbVector2 direction;
+        public float speed;
+        public SpacetimeDB.Timestamp last_split_time;
+    }
+
+    [Table(Name = "food", Public = true)]
+    public partial struct Food
+    {
+        [PrimaryKey]
+        public uint entity_id;
+    }
+
+    [Table(Name = "player", Public = true)]
+    public partial struct Player
+    {
+        [PrimaryKey]
+        public Identity identity;
+        [Unique, AutoInc]
+        public uint player_id;
+        public string name;
+    }
+
+    [Reducer(ReducerKind.ClientConnected)]
+public static void Connect(ReducerContext ctx)
+{
+    Log.Info($"{ctx.Sender} just connected.");
+}
+
+
+}
+
+// This allows us to store 2D points in tables.
+[SpacetimeDB.Type]
+public partial struct DbVector2
+{
+    public float x;
+    public float y;
+
+    public DbVector2(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
